6bd5da2b6d2982a1580cc36105cb987f5dc75027
unknown <umerarif01@gmail.com>
2021-07-17 18:38:35 +0500

    GOT BOOKS API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  
+  const [books, setBooks] =  useState(null);
+
+    const apiURL = "https://www.anapioficeandfire.com/api/books?pageSize=30";
+
+    const fetchData = async () => {
+        const response = await axios.get(apiURL)
+
+        setBooks(response.data) 
+        console.log(response.data);
+    }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <center>
+    <h1>Games of Thrones Books Api ⚔️</h1>
+<button onClick={fetchData}>Fetch Data</button>
+</center>
+
+<div className="books">
+        {books &&
+          books.map((book, index) => {
+            const cleanedDate = new Date(book.released).toDateString();
+            const authors = book.authors.join(', ');
+
+            return (
+              <center>
+              <div className="book" key={index}>
+                <h3>Book {index + 1}</h3>
+                <h2>{book.name}</h2>
+
+                <div className="details">
+                  <p>👨: {authors}</p>
+                  <p>📖: {book.numberOfPages} pages</p>
+                  <p>🏘️: {book.country}</p>
+                  <p>⏰: {cleanedDate}</p>
+                  <p>📕: {book.publisher}</p>
+                </div>
+              </div>
+              </center>
+            );
+          })}
+     
+      </div>
+    </>
   );
 }
 
